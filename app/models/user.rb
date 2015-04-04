547818becb80ff4f19664451df74b42ba6f7db1d
Dicko Sow <s12.dsow@gmail.com>
2015-04-03 21:48:53 -0400

    added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-
-	has_secure_password
-	
-	has_many :user_dramas
-	has_many :dramas, through: :user_dramas
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
