e2b66700883b2597a746b26f08e5081b091ef6ee
Dicko Sow <s12.dsow@gmail.com>
2015-05-22 13:01:33 -0400

    changed seeds file to include yml file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-
-dicko = User.create(username: 's12dsow', password: "password")
-
-matthew = User.create(username: "mmwho", password: "password")
-
-coffee_prince = Drama.create(name: "Coffee Prince", summary: "boy meets girl which he thinks is a boy", content_url: "https://youtu.be/94UkVK4SyJQ")
-
-ditto = Drama.create(name: "ditto", summary: "time traveling love that is not meant to be", content_url: "https://youtu.be/DrCn-HELAxE")
-
-dicko_drama = UserDrama.create(user_id: 1, drama_id: 1, watched?: true, rating: 5, review: "loveeeed it!" )
-matthew_drama = UserDrama.create(user_id: 2, drama_id: 2, watched?: true, rating: 5, review: "clearly loved it")
-
+seeds = YAML.load_file(Rails.root.join("db", "seeds.yml"))
+users = User.create(seeds["user"])
+dramas = Drama.create(seeds["drama"])
 
