af4059f4f22641e3271bd1e4aaf4a3f0f8508919
Dicko Sow <s12.dsow@gmail.com>
2015-05-22 01:53:19 -0400

    changes to login spec to fix rubocop errors

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,23 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 feature "Signing in" do
   given(:user) { FactoryGirl.create(:user) }
 
   scenario "Signing in with correct credentials" do
     visit new_user_session_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
     visit root_path
-    expect(page).to have_content 'Welcome dramaLovers!'
+    expect(page).to have_content "Welcome dramaLovers!"
   end
 
   scenario "Signing in with incorrect credentials" do
     visit new_user_session_path
-    fill_in 'Email', with: "wrong@email.com"
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
+    fill_in "Email", with: "wrong@email.com"
+    fill_in "Password", with: user.password
+    click_button "Log in"
     visit root_path
-    expect(page).to have_content 'Login'
+    expect(page).to have_content "Login"
   end
 end
