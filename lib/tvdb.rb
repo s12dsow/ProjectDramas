6af7abdd578162ff2baca3294582c21c942071cb
Dicko Sow <s12.dsow@gmail.com>
2015-05-22 02:00:17 -0400

    fixes to tvdb file to fix rubocop errors

--- lib/tvdb.rb
+++ lib/tvdb.rb
@@ -1,28 +1,28 @@
-require 'httparty'
+require "httparty"
 
 module TVDB
-  API_KEY  = '83D9DB7DA81B26FF'
-  BASE_URL = 'http://www.thetvdb.com/api/'
+  API_KEY = "83D9DB7DA81B26FF"
+  BASE_URL = "http://www.thetvdb.com/api/"
 
   def self.build_drama(series_id)
     xml = get(series_url(series_id))
 
-    drama = parse_drama(xml["Data"]["Series"])
+    parse_drama(xml["Data"]["Series"])
   end
 
   def self.parse_drama(drama_xml)
-    name   = drama_xml['SeriesName']
-    summary = drama_xml['Overview']
-    image  = "http://www.thetvdb.com/banners/" + (drama_xml['poster'] || drama_xml['banner']).to_s
-    tvdbid = drama_xml['id']
+    name = drama_xml["SeriesName"]
+    summary = drama_xml["Overview"]
+    image = "http://www.thetvdb.com/banners/" + (drama_xml["poster"] || drama_xml["banner"]).to_s
+    tvdbid = drama_xml["id"]
 
     drama = Drama.find_or_create_by(tvdbid: tvdbid)
     drama.update_attributes(name: name, summary: summary, image: image)
-    return drama
+    drama
   end
 
   def self.get(url)
-    get_url = HTTParty.get(url)
+    HTTParty.get(url)
   end
 
   def self.build_authenticated_url(path)
@@ -30,7 +30,7 @@ module TVDB
   end
 
   def self.series_url(series_id)
-    build_authenticated_url('/series/' + series_id.to_s + '/all/en.xml')
+    build_authenticated_url("/series/" + series_id.to_s + "/all/en.xml")
   end
 
   def self.build_url(path)
@@ -38,11 +38,10 @@ module TVDB
   end
 
   def self.search(query)
-    terms = query.split(' ').join("%20")
+    terms = query.split(" ").join("%20")
 
     xml = get(build_url("/GetSeries.php?seriesname=" + terms))
-  
-    result = xml["Data"].nil? ? []  : [xml["Data"]["Series"]]
+    result = xml["Data"].nil? ? [] : [xml["Data"]["Series"]]
     result.flatten.map do |drama|
       parse_drama(drama)
     end
