780dcd50b6598cd8db2aecde79c545788c442354
Dicko Sow <s12.dsow@gmail.com>
2015-05-27 23:45:31 -0400

    removed unneeded key

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ module ProjectDramas
     # config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
     # config.i18n.default_locale = :de
 
-    config.secret_token = ENV["SECRET_KEY_BASE"]
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
